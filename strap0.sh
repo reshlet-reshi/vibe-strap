0ec128f3e4d9709af2cffc73268f7d9098874fa7
Matthew Draper <draper_matthewjohn@yahoo.com>
2026-04-26 14:23:37 -0700

    Make stage0 position independent

--- strap0.sh
+++ strap0.sh
@@ -11,15 +11,13 @@ emit_hex 00             # e_ident[EI_OSABI]: ELFOSABI_SYSV
 emit_hex 00             # e_ident[EI_ABIVERSION]: 0
 emit_hex 00 00 00 00 	# e_ident[EI_PAD..]: padding
 emit_hex 00 00 00       # ...
-emit_hex 02 00          # e_type = 2; ET_EXEC, executable file
+emit_hex 03 00          # e_type = 3; ET_DYN, shared object / PIE
 emit_hex 03 00          # e_machine = 3; EM_386, Intel 80386
 emit_hex 01 00 00 00    # e_version = 1; EV_CURRENT
 
-# e_entry points at the first instruction:
-#   base vaddr  0x08048000 (p_vaddr in the program header)
-# + code offset       0x54 (52-byte ELF header + 32-byte program header)
-# = entry vaddr 0x08048054
-emit_hex 54 80 04 08
+emit_hex 54 00 00 00    # e_entry = 0x54;
+                        #  relative vaddr of first code byte.
+                        #  skips 52-byte ELF header + 32-byte program header.
 
 # e_phoff = 52; e_shoff = 0; e_flags = 0.
 emit_hex 34 00 00 00 00 00 00 00 00 00 00 00
@@ -27,21 +25,26 @@ emit_hex 34 00 00 00 00 00 00 00 00 00 00 00
 emit_hex 34 00 20 00 01 00 00 00 00 00 00 00
 
 # Program header: one RX PT_LOAD segment containing the whole file.
-# p_filesz/p_memsz = 133 bytes.
-emit_hex 01 00 00 00 00 00 00 00 00 80 04 08 00 80 04 08
-emit_hex 85 00 00 00 85 00 00 00 05 00 00 00 00 10 00 00
+# p_vaddr = 0; p_filesz/p_memsz = 136 bytes.
+emit_hex 01 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+emit_hex 88 00 00 00 88 00 00 00 05 00 00 00 00 10 00 00
 
-# Code at file offset 84 / vaddr 0x08048054.
+# Code at file offset 84 / relative vaddr 0x54.
+#   jmp msgref
+# start:
+#   pop ecx             ; msg
 #   mov eax, 4          ; sys_write
 #   mov ebx, 1          ; stdout
-#   mov ecx, msg
 #   mov edx, 18
 #   int 0x80
 #   mov eax, 1          ; sys_exit
 #   xor ebx, ebx
 #   int 0x80
-emit_hex b8 04 00 00 00 bb 01 00 00 00 b9 73 80 04 08
-emit_hex ba 12 00 00 00 cd 80 b8 01 00 00 00 31 db cd 80
+# msgref:
+#   call start
+emit_hex eb 1b 59 b8 04 00 00 00 bb 01 00 00 00 ba 12 00
+emit_hex 00 00 cd 80 b8 01 00 00 00 31 db cd 80 e8 e0 ff
+emit_hex ff ff
 
 emit_raw 'vibe-strap stage0'
 emit_hex 0a
