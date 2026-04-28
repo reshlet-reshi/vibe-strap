4e5cc6a89166259896087fb52eaa5d44ea4f8ae7
Matthew Draper <reshlet.reshi@gmail.com>
2026-04-28 09:25:49 -0700

    Move shell sources under src

--- tests/exit1/exit1.sh
+++ tests/exit1/exit1.sh
@@ -1,8 +1,8 @@
 # shellcheck shell=sh
-. ./std/elf/begin.sh
+. ./src/std/elf/begin.sh
 
 emit_hex b8 01 00 00 00 #   mov eax, 1      ; syscall: exit
 emit_hex bb 01 00 00 00 #   mov ebx, 1      ; status: 1
 emit_hex cd 80          #   int 0x80        ; exit(1)
 
-. ./std/elf/end.sh
+. ./src/std/elf/end.sh
--- tests/hello/hello.sh
+++ tests/hello/hello.sh
@@ -1,12 +1,12 @@
 # shellcheck shell=sh
-. ./std/elf/begin.sh
+. ./src/std/elf/begin.sh
 
-. ./std/str/begin.sh
+. ./src/std/str/begin.sh
 emit_raw 'Hello World!' #   "Hello World!"
 emit_hex 0a             #   "\n"
-. ./std/str/end.sh
+. ./src/std/str/end.sh
 
-# ./std/str/begin+end leaves:
+# ./src/std/str/begin+end leaves:
 #  - str addr in ecx
 #  - str len in edx
 emit_hex b8 04 00 00 00 #   mov eax, 4      ; syscall: write
@@ -16,4 +16,4 @@ emit_hex b8 01 00 00 00 #   mov eax, 1      ; syscall: exit
 emit_hex 31 db          #   xor ebx, ebx    ; status: 0
 emit_hex cd 80          #   int 0x80        ; exit(0)
 
-. ./std/elf/end.sh
+. ./src/std/elf/end.sh
