ead2b3187def63bbbcb1f59b4f0526f740850fe4
reshi <reshlet.reshi@gmail.com>
2026-04-29 00:50:51 -0700

    Trim bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,9 @@
 #!/bin/sh
 
-# "strict mode"
 set -eu
 
-# DEBUG : always clean for now
 rm -rf ./.ignore ./runme
-# LATER
-#
-# already bootstrapped? just exec runme!
-#if test -f runme; then
-#    exec ./runme
-#fi
-#
 
-# platform check
 platform="$(uname -m)-$(uname -s)"
 expected='x86_64-Linux'
 if test "$platform" != "$expected"; then
@@ -21,7 +11,6 @@ if test "$platform" != "$expected"; then
     exit 1
 fi
 
-# tmp dir to unpack musl cc
 tmp="$(mktemp -d)"
 cleanup() {
     rm -rf "$tmp"
@@ -29,17 +18,13 @@ cleanup() {
 trap 'cleanup' EXIT
 trap 'cleanup; exit 130' HUP INT TERM
 
-# unpack cc
 muslcc_archive="./vendor/muslcc/x86_64-linux-musl-cross.tgz"
 tar -xzf "$muslcc_archive" -C "${tmp}"
 
-# cc runme.c
 cc="${tmp}/x86_64-linux-musl-cross/bin/x86_64-linux-musl-cc"
 "$cc" -static -o ./runme ./runme.c
 
-# clean tmp
 cleanup
 trap - EXIT HUP INT TERM
 
-# make rocket go now
 exec ./runme
